50d295e4cb1e21f657a0736b24ec376160663f3f
32242WatanabeTakumi-EE <32242@toyota.kosen-ac.jp>
2025-12-10 11:41:27 +0900

    add pose.py

---
+++ Pose/pose.py
@@ -0,0 +1,22 @@
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+
+model_path = 'C:\Users\TAKUMI WATANABE\Desktop\Zemi2\PythonDev\python-3.12.10-embed-amd64\hand_model\hand_landmarker.task'
+
+BaseOptions = mp.tasks.BaseOptions
+HandLandmarker = mp.tasks.vision.HandLandmarker
+HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
+HandLandmarkerResult = mp.tasks.vision.HandLandmarkerResult
+VisionRunningMode = mp.tasks.vision.RunningMode
+
+# Create a hand landmarker instance with the live stream mode:
+def print_result(result: HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
+    print('hand landmarker result: {}'.format(result))
+
+options = HandLandmarkerOptions(
+    base_options=BaseOptions(model_asset_path='/path/to/model.task'),
+    running_mode=VisionRunningMode.LIVE_STREAM,
+    result_callback=print_result)
+with HandLandmarker.create_from_options(options) as landmarker:
+    no
