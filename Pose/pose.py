2633a1f989eb6b58b6b985c17ccc1e5ee6f6c548
katsumasa <katsumasa-abe@outlook.jp>
2025-12-17 11:31:34 +0900

    骨格推定のベース作成

--- Pose/pose.py
+++ Pose/pose.py
@@ -2,21 +2,22 @@ import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 
-model_path = 'C:\Users\TAKUMI WATANABE\Desktop\Zemi2\PythonDev\python-3.12.10-embed-amd64\hand_model\hand_landmarker.task'
+model_path = './Model/pose_landmarker_full.task'
 
 BaseOptions = mp.tasks.BaseOptions
-HandLandmarker = mp.tasks.vision.HandLandmarker
-HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
-HandLandmarkerResult = mp.tasks.vision.HandLandmarkerResult
+PoseLandmarker = mp.tasks.vision.PoseLandmarker
+PoseLandmarkerOptions = mp.tasks.vision.PoseLandmarkerOptions
+PoseLandmarkerResult = mp.tasks.vision.PoseLandmarkerResult
 VisionRunningMode = mp.tasks.vision.RunningMode
 
-# Create a hand landmarker instance with the live stream mode:
-def print_result(result: HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
-    print('hand landmarker result: {}'.format(result))
+# Create a pose landmarker instance with the live stream mode:
+def print_result(result: PoseLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
+    print('pose landmarker result: {}'.format(result))
 
-options = HandLandmarkerOptions(
-    base_options=BaseOptions(model_asset_path='/path/to/model.task'),
+options = PoseLandmarkerOptions(
+    base_options=BaseOptions(model_asset_path=model_path),
     running_mode=VisionRunningMode.LIVE_STREAM,
     result_callback=print_result)
-with HandLandmarker.create_from_options(options) as landmarker:
-    no
+
+with PoseLandmarker.create_from_options(options) as landmarker:
+    pass
